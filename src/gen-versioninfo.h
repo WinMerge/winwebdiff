69b0f225b231c9eed510643faf5e129a5663ba94
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2022-04-26 00:12:35 +0900

    Bump revision to 1.0.1

--- src/gen-versioninfo.h
+++ src/gen-versioninfo.h
@@ -1,7 +1,7 @@
 //GIT-VS-VERSION-GEN.bat generated resource header.
-#define GEN_VER_VERSION_STRING   "1.0.0.0\0" 
-#define GEN_VER_DIGITAL_VERSION  1,0,0,0 
-#define GEN_VER_VERSION_HEX      0x0001000000000000 
+#define GEN_VER_VERSION_STRING   "1.0.1.0\0" 
+#define GEN_VER_DIGITAL_VERSION  1,0,1,0 
+#define GEN_VER_VERSION_HEX      0x0001000000010000 
 #define GEN_VER_COMMENT_STRING   "Major Version Release\0" 
 #define GEN_VER_PRIVATE_FLAG     VS_FF_PRIVATEBUILD 
 #define GEN_VER_PRIVATE_STRING   "Custom Build\0" 
