911ff608c471c83908ee3294d5b85c46e433f881
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2024-01-14 21:26:49 +0900

    WIP

--- src/WinWebDiffLib/Resource.h
+++ src/WinWebDiffLib/Resource.h
@@ -2,7 +2,6 @@
 // Microsoft Visual C++ generated include file.
 // Used by WinWebDiffLib.rc
 //
-#define GEN_VER_PRERELEASE_FLAG         0
 #define ID_GOBACK                       100
 #define ID_GOFORWARD                    101
 #define ID_RELOAD                       102
@@ -53,9 +52,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1031
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
